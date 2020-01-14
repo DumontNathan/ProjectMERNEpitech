3e84b43b035e6dc2d6dc8337bb91f8cab8b70d98
nathan <nathu.dumont@gmail.com>
2020-01-14 17:21:17 +0100

    login/register/showUsers

--- client/src/Components/Actions/UsersList.js
+++ client/src/Components/Actions/UsersList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 class UsersList extends Component {
@@ -19,28 +19,30 @@ class UsersList extends Component {
 
   render() {
     return (
-      <div className="container center">
-        <div class="row">
-          <div class="col s12 m3">
-            <Link
-              to="/dashboard"
-              className="btn-flat waves-effect white-text text-darken-1"
-            >
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              dashboard
-            </Link>
-            {this.state.users.map(user => (
-              <div class="card teal darken-1">
-                <div class="card-content white-text">
-                  <span class="card-title">{user.username}</span>
-                  <p>{user.email}</p>
+      <div className="container">
+        <div className="container">
+          <div class="row container">
+            <div class="container">
+              <Link
+                to="/dashboard"
+                className="btn-flat waves-effect white-text text-darken-1"
+              >
+                <i className="material-icons left">keyboard_backspace</i> Back
+                to dashboard
+              </Link>
+              {this.state.users.map(user => (
+                <div class="card teal darken-1">
+                  <div class="card-content white-text">
+                    <span class="card-title">{user.username}</span>
+                    <p>{user.email}</p>
+                  </div>
+                  <div class="card-action">
+                    <a href="">Follow</a>
+                    <a href="">Check profile</a>
+                  </div>
                 </div>
-                <div class="card-action">
-                  <a href="">Follow</a>
-                  <a href="">Check profile</a>
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
